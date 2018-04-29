917c52967386a22f7b2f4a59925453768b180ba2
Kenji Hanamoto <khanamoto10@gmail.com>
2018-04-30 00:55:41 +0900

    refactor: Update until front-end settings

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,21 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require turbolinks
+//= require_tree .
+
+// 以下Bootstrap用
+//= require jquery3
+//= require popper
+// bootstrap-sprockets はデバッグ用。本番では軽量な bootstrap にする
+//= require bootstrap-sprockets
