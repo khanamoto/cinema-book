0d758c07d738741aaff245d18d2e700a3787fc57
Kenji Hanamoto <khanamoto10@gmail.com>
2018-05-03 16:20:18 +0900

    refactor: Move some settings

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,5 @@ module Films
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    config.time_zone = 'Tokyo'
-    config.generators.template_engine = :slim
   end
 end
