1862f9e879c1b4c6bcbf81fd0b94f3b1d979c541
Kenji Hanamoto <khanamoto10@gmail.com>
2018-05-07 23:20:19 +0900

    feat: Add basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+  def basic_auth
+    # 認証に使うユーザ名とパスワードは環境変数に書く
+    authenticate_or_request_with_http_basic do |user, pass|
+      user == ENV["BASIC_AUTH_USER"] && Digest::SHA1.hexdigest(pass) == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
