603cf3127303ee2b690c31de68a81ac24217f92a
Kenji Hanamoto <khanamoto10@gmail.com>
2018-05-14 01:25:40 +0900

    feat: Add html5 form validation (not use js)"

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,7 +11,7 @@ class RegistrationsController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to '/'
+      redirect_to :root
     else
       render :new
     end
