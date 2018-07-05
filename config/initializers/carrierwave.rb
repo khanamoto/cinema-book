7c7897830eed2e2219da2217a0a97c640622abb2
Kenji Hanamoto <khanamoto10@gmail.com>
2018-07-05 17:14:58 +0900

    refactor: Disable in development

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,18 +1,18 @@
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
+# CarrierWave.configure do |config|
+#   config.fog_provider = 'fog/aws'
 
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ENV['AWS_REGION']
-  }
+#   config.fog_credentials = {
+#     provider: 'AWS',
+#     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+#     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+#     region: ENV['AWS_REGION']
+#   }
 
-  config.fog_directory = ENV['AWS_S3_BUCKET']
-  config.asset_host = ENV['AWS_S3_URL']
-  # キャッシュもS3に置く
-  config.cache_storage = :fog
+#   config.fog_directory = ENV['AWS_S3_BUCKET']
+#   config.asset_host = ENV['AWS_S3_URL']
+#   # キャッシュもS3に置く
+#   config.cache_storage = :fog
 
-  # 日本語の文字化けを防ぐ
-  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
-end
+#   # 日本語の文字化けを防ぐ
+#   CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+# end
