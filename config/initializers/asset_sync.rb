0370e091e0df673fc6f1796f83ed12fa8b0873c0
Kenji Hanamoto <khanamoto10@gmail.com>
2018-07-07 23:15:14 +0900

    refactor: Build

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,8 +1,8 @@
-AssetSync.configure do |config|
-  config.fog_provider = 'AWS'
-  config.fog_directory = ENV['AWS_S3_BUCKET']
-  config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
-  config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-  config.manifest = true
-  config.fog_region = ENV['AWS_REGION']
-end
+# AssetSync.configure do |config|
+#   config.fog_provider = 'AWS'
+#   config.fog_directory = ENV['AWS_S3_BUCKET']
+#   config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
+#   config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+#   config.manifest = true
+#   config.fog_region = ENV['AWS_REGION']
+# end
