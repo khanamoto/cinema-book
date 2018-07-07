526c1668a38c67dbad511bd80cfc7465b74f9512
Kenji Hanamoto <khanamoto10@gmail.com>
2018-07-08 03:14:28 +0900

    refactor: Display timeline when not logined

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -1,6 +1,4 @@
 class TimelineController < ApplicationController
-  before_action :check_logined
-
   def index
     @films = Film.all.order(created_at: :desc).page(params[:page]).per(10).search(params[:search])
   end
