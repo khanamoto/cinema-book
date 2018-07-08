814e026f6e76b3d4e44a4de1657625845803121c
Kenji Hanamoto <khanamoto10@gmail.com>
2018-07-08 13:26:39 +0900

    refactor: Update Asset Sync

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,8 +1,10 @@
-# AssetSync.configure do |config|
-#   config.fog_provider = 'AWS'
-#   config.fog_directory = ENV['AWS_S3_BUCKET']
-#   config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
-#   config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-#   config.manifest = true
-#   config.fog_region = ENV['AWS_REGION']
-# end
+if defined?(AssetSync)
+  AssetSync.configure do |config|
+    config.fog_provider = 'AWS'
+    config.fog_directory = ENV['AWS_S3_BUCKET']
+    config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
+    config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+    config.manifest = true
+    config.fog_region = ENV['AWS_REGION']
+  end
+end
