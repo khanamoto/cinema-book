a7515fa0f574a7910c02eff4ddc1b6920f468a15
Kenji Hanamoto <khanamoto10@gmail.com>
2018-07-12 17:54:33 +0900

    refactor: Display search result text

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -1,5 +1,8 @@
 class TimelineController < ApplicationController
   def index
+    # 検索結果一覧を取得する
     @films = Film.all.order(created_at: :desc).page(params[:page]).per(10).search(params[:search])
+    # 検索件数を取得する
+    @films_count = Film.search(params[:search]).count
   end
 end
